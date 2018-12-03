748c11e9543b4a7669fa4275fd2b804299e769ca
KostaRudenko <goglemoogle@gmail.com>
2018-12-03 23:13:21 +0200

    up

--- main.js
+++ main.js
@@ -5,43 +5,60 @@ let newGame = document.getElementById('newGame');
 
 let step = 0;
 
-game.addEventListener('click', (e)=> {
+game.addEventListener('click', (e) => {
     if (e.target.className === 'cell') {
-        if(step % 2 === 0) e.target.innerHTML = 'x';
-         else e.target.innerHTML = 'o';
+        if (step % 2 === 0) {
+            e.target.innerHTML = 'x';
+            result.innerHTML = 'Ходят Нолики';
+        } else {
+            e.target.innerHTML = 'o';
+            result.innerHTML = 'Ходят Крестики';
+        }
 
         step++;
         checkWinner();
-
-        if (step === 9) result.innerHTML = 'Its a Draw';
+        //if (step === 9) result.innerHTML = 'Ничья';
 
         if (e.target.textContent === 'x') e.target.classList.add('x');
-         else if (e.target.textContent === 'o') e.target.classList.add('o');
+        if (e.target.textContent === 'o') e.target.classList.add('o');
     }
+    blk();
 });
 
-let checkWinner =()=> {
-    // X
-    if (cell[0].innerHTML === 'x' && cell[1].innerHTML === 'x' && cell[2].innerHTML === 'x') result.innerHTML = `Player 'X' win!`;
-    if (cell[3].innerHTML === 'x' && cell[4].innerHTML === 'x' && cell[5].innerHTML === 'x') result.innerHTML = `Player 'X' win!`;
-    if (cell[6].innerHTML === 'x' && cell[7].innerHTML === 'x' && cell[8].innerHTML === 'x') result.innerHTML = `Player 'X' win!`;
-    if (cell[0].innerHTML === 'x' && cell[3].innerHTML === 'x' && cell[6].innerHTML === 'x') result.innerHTML = `Player 'X' win!`;
-    if (cell[1].innerHTML === 'x' && cell[4].innerHTML === 'x' && cell[7].innerHTML === 'x') result.innerHTML = `Player 'X' win!`;
-    if (cell[2].innerHTML === 'x' && cell[5].innerHTML === 'x' && cell[8].innerHTML === 'x') result.innerHTML = `Player 'X' win!`;
-    if (cell[0].innerHTML === 'x' && cell[4].innerHTML === 'x' && cell[8].innerHTML === 'x') result.innerHTML = `Player 'X' win!`;
-    if (cell[2].innerHTML === 'x' && cell[4].innerHTML === 'x' && cell[6].innerHTML === 'x') result.innerHTML = `Player 'X' win!`;
-    // O
-    if (cell[0].innerHTML === 'o' && cell[1].innerHTML === 'o' && cell[2].innerHTML === 'o') result.innerHTML = `Player 'O' win!`;
-    if (cell[3].innerHTML === 'o' && cell[4].innerHTML === 'o' && cell[5].innerHTML === 'o') result.innerHTML = `Player 'O' win!`;
-    if (cell[6].innerHTML === 'o' && cell[7].innerHTML === 'o' && cell[8].innerHTML === 'o') result.innerHTML = `Player 'O' win!`;
-    if (cell[0].innerHTML === 'o' && cell[3].innerHTML === 'o' && cell[6].innerHTML === 'o') result.innerHTML = `Player 'O' win!`;
-    if (cell[1].innerHTML === 'o' && cell[4].innerHTML === 'o' && cell[7].innerHTML === 'o') result.innerHTML = `Player 'O' win!`;
-    if (cell[2].innerHTML === 'o' && cell[5].innerHTML === 'o' && cell[8].innerHTML === 'o') result.innerHTML = `Player 'O' win!`;
-    if (cell[0].innerHTML === 'o' && cell[4].innerHTML === 'o' && cell[8].innerHTML === 'o') result.innerHTML = `Player 'O' win!`;
-    if (cell[2].innerHTML === 'o' && cell[4].innerHTML === 'o' && cell[6].innerHTML === 'o') result.innerHTML = `Player 'O' win!`;
+let blk = () => {
+    if (result.innerHTML === 'Крестики Выиграли!') {
+        for (let i = 0; i < cell.length; i++) {
+            if (cell[i].classList.length === 1) cell[i].classList.add('blank');
+        }
+    } else if (result.innerHTML === 'Нолики Выиграли!') {
+        for (let i = 0; i < cell.length; i++) {
+            if (cell[i].classList.length === 1) cell[i].classList.add('blank');
+        }
+    }
 };
 
-newGame.addEventListener('click', ()=> {
+let checkWinner = () => {
+    // X
+    if (cell[0].innerHTML === 'x' && cell[1].innerHTML === 'x' && cell[2].innerHTML === 'x') result.innerHTML = 'Крестики Выиграли!';
+    if (cell[3].innerHTML === 'x' && cell[4].innerHTML === 'x' && cell[5].innerHTML === 'x') result.innerHTML = 'Крестики Выиграли!';
+    if (cell[6].innerHTML === 'x' && cell[7].innerHTML === 'x' && cell[8].innerHTML === 'x') result.innerHTML = 'Крестики Выиграли!';
+    if (cell[0].innerHTML === 'x' && cell[3].innerHTML === 'x' && cell[6].innerHTML === 'x') result.innerHTML = 'Крестики Выиграли!';
+    if (cell[1].innerHTML === 'x' && cell[4].innerHTML === 'x' && cell[7].innerHTML === 'x') result.innerHTML = 'Крестики Выиграли!';
+    if (cell[2].innerHTML === 'x' && cell[5].innerHTML === 'x' && cell[8].innerHTML === 'x') result.innerHTML = 'Крестики Выиграли!';
+    if (cell[0].innerHTML === 'x' && cell[4].innerHTML === 'x' && cell[8].innerHTML === 'x') result.innerHTML = 'Крестики Выиграли!';
+    if (cell[2].innerHTML === 'x' && cell[4].innerHTML === 'x' && cell[6].innerHTML === 'x') result.innerHTML = 'Крестики Выиграли!';
+    // O
+    if (cell[0].innerHTML === 'o' && cell[1].innerHTML === 'o' && cell[2].innerHTML === 'o') result.innerHTML = 'Нолики Выиграли!';
+    if (cell[3].innerHTML === 'o' && cell[4].innerHTML === 'o' && cell[5].innerHTML === 'o') result.innerHTML = 'Нолики Выиграли!';
+    if (cell[6].innerHTML === 'o' && cell[7].innerHTML === 'o' && cell[8].innerHTML === 'o') result.innerHTML = 'Нолики Выиграли!';
+    if (cell[0].innerHTML === 'o' && cell[3].innerHTML === 'o' && cell[6].innerHTML === 'o') result.innerHTML = 'Нолики Выиграли!';
+    if (cell[1].innerHTML === 'o' && cell[4].innerHTML === 'o' && cell[7].innerHTML === 'o') result.innerHTML = 'Нолики Выиграли!';
+    if (cell[2].innerHTML === 'o' && cell[5].innerHTML === 'o' && cell[8].innerHTML === 'o') result.innerHTML = 'Нолики Выиграли!';
+    if (cell[0].innerHTML === 'o' && cell[4].innerHTML === 'o' && cell[8].innerHTML === 'o') result.innerHTML = 'Нолики Выиграли!';
+    if (cell[2].innerHTML === 'o' && cell[4].innerHTML === 'o' && cell[6].innerHTML === 'o') result.innerHTML = 'Нолики Выиграли!';
+};
+
+newGame.addEventListener('click', () => {
     location.reload();
 });
 
